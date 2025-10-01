27f66592882e142ede93ce5470831d3951c13126
Facundo Méndez Bentos <facumendezbt@gmail.com>
2025-09-30 22:24:04 -0300

    Update types.ts

--- model/types.ts
+++ model/types.ts
@@ -6,56 +6,9 @@ export interface BigPotatoDao<T, ID> {
     delete(id: ID): Promise<void>;
 }
 
-// Entities
-export interface Customer {
-    id: number;
-    name: string;
-    email: string;
-    phone?: string;
-    address: string;
-}
-
-export interface Category {
-    id: number;
-    name: string;
-    description?: string;
-}
-
-export interface Product {
-    id: number;
-    name: string;
-    description: string;
-    price: number;
-    stock: number;
-    category: Category[];
-}
-
-export interface Order {
-    id: number;
-    customerId: number;
-    orderDate: Date;
-    status: OrderStatus;
-    totalAmount: number;
-    items: OrderItem[];
-    updatedAt: Date;
-}
-
-// 3x monitor samsung + $500
-// 3x monitor samsung - $50 (discount)
-
 export enum OrderItemType {
     PRODUCT = 'PRODUCT',
     DISCOUNT = 'DISCOUNT',
-}
-
-export interface OrderItem {
-    id: number;
-    orderId: number;
-    type: OrderItemType;
-    productId: number;
-    quantity: number;
-    unitPrice: number; // Positive for products, negative for discounts
-    subtotal: number;
 }
 
 export enum OrderStatus {
